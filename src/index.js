464d04b1daf70ea7b038f999ff909af61f637cfe
KatherineVito <katyjulio16@gmail.com>
2022-10-12 13:15:03 -0500

    example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { getAllByPlaceholderText } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Greeting, UserCard } from "./Greeting";
+import Product, { Navbar } from "./Product";
+import { Button } from "./Button";
+import {Example} from './Example';
+import {Ejemplo} from './Input';
+import {Prueba} from './State'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+  <Prueba/>
+  {/* <Example name='Hola mundo' lastname={true}/> */}
+  {/* <Ejemplo /> */}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  {/* <Button text='Click me'/>
+  <Button text='Pay'/>
+  <Button text='' name ='Joe'/> */}
+   {/*  <UserCard
+      name="Ana Perez"
+      amount={3000}
+      married={true}
+      points={[90, 22, 2]}
+      address={{ street: "123 Main Street", city: "New York" }}
+      greet={(function () {getAllByPlaceholderText('hello')})}
+    /> */}
+  </>
+)
